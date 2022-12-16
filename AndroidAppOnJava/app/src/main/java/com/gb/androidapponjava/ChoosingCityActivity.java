3cdffb81e80daaebe2d3feb845abeae9e8363ceb
buntlit <buntlit@mail.ru>
2022-12-16 17:45:38 +0300

    Homework Lesson 4

--- AndroidAppOnJava/app/src/main/java/com/gb/androidapponjava/ChoosingCityActivity.java
+++ AndroidAppOnJava/app/src/main/java/com/gb/androidapponjava/ChoosingCityActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ChoosingCityActivity extends AppCompatActivity {
 
     final ChooseCityPresenter presenter = ChooseCityPresenter.getInstance();
+    final String CITY = "CITY";
+    final String HUMIDITY = "HUMIDITY";
+    final String PRESSURE = "PRESSURE";
+    final String WIND_SPEED  ="WIND_SPEED";
 
 
     @Override
@@ -24,12 +29,38 @@ public class ChoosingCityActivity extends AppCompatActivity {
         CheckBox checkBoxPressure = findViewById(R.id.checkBoxPressure);
         CheckBox checkBoxWindSpeed = findViewById(R.id.checkBoxWindSpeed);
         restoreInstance(enterCity, checkBoxHumidity, checkBoxPressure, checkBoxWindSpeed);
+
+        TextView textViewCity1 = findViewById(R.id.city1);
+        textViewCity1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String city = textViewCity1.getText().toString();
+                showWeather(city, checkBoxHumidity.isChecked(), checkBoxPressure.isChecked(), checkBoxWindSpeed.isChecked());
+            }
+        });
+        TextView textViewCity2 = findViewById(R.id.city2);
+        textViewCity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String city = textViewCity2.getText().toString();
+                showWeather(city, checkBoxHumidity.isChecked(), checkBoxPressure.isChecked(), checkBoxWindSpeed.isChecked());
+            }
+        });
+        TextView textViewCity3 = findViewById(R.id.city3);
+        textViewCity3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String city = textViewCity3.getText().toString();
+                showWeather(city, checkBoxHumidity.isChecked(), checkBoxPressure.isChecked(), checkBoxWindSpeed.isChecked());
+            }
+        });
+
         Button showWeatherButton = findViewById(R.id.buttonShowWeather);
         showWeatherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                presenter.setEnterCity(enterCity.getText().toString());
-                startActivity(new Intent(ChoosingCityActivity.this, MainActivity.class));
+                String city = enterCity.getText().toString();
+                showWeather(city, checkBoxHumidity.isChecked(), checkBoxPressure.isChecked(), checkBoxWindSpeed.isChecked());
             }
         });
     }
@@ -66,5 +97,14 @@ public class ChoosingCityActivity extends AppCompatActivity {
         checkBoxHumidity.setChecked(presenter.getIsCheckBoxHumidity());
         checkBoxPressure.setChecked(presenter.getIsCheckBoxPressure());
         checkBoxWindSpeed.setChecked(presenter.getIsCheckBoxWindSpeed());
+    }
+
+    private void showWeather(String city, boolean isCheckedHumidity, boolean isCheckedPressure, boolean isCheckedWindSpeed){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(CITY, city);
+        intent.putExtra(HUMIDITY, isCheckedHumidity);
+        intent.putExtra(PRESSURE, isCheckedPressure);
+        intent.putExtra(WIND_SPEED, isCheckedWindSpeed);
+        startActivity(intent);
     }
 }
--- AndroidAppOnJava/app/src/main/java/com/gb/androidapponjava/MainActivity.java
+++ AndroidAppOnJava/app/src/main/java/com/gb/androidapponjava/MainActivity.java
@@ -4,22 +4,29 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String name;
     final String TAG = "MainActivity";
+    final String CITY = "CITY";
+    final String HUMIDITY = "HUMIDITY";
+    final String PRESSURE = "PRESSURE";
+    final String WIND_SPEED = "WIND_SPEED";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String instanceState;
         name = " - onCreate";
+        restoreInstance();
         Button settingsButton = findViewById(R.id.buttonSettings);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SettingsActivity.class));
             }
         });
+        Button showWeatherOnInternet = findViewById(R.id.showWeatherOnInternet);
+        showWeatherOnInternet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri address = Uri.parse("https://yandex.ru/pogoda/korolev");
+                startActivity(new Intent(Intent.ACTION_VIEW, address));
+            }
+        });
+
+//        String instanceState;
 //        if (savedInstanceState == null) {
 //            instanceState = "First run";
 //        } else {
@@ -98,5 +115,32 @@ public class MainActivity extends AppCompatActivity {
         name = "onDestroy";
         Toast.makeText(getApplicationContext(), TAG + " " + name, Toast.LENGTH_SHORT).show();
         Log.d(TAG, name);
+    }
+
+    private void restoreInstance() {
+        String city = getIntent().getExtras().getString(CITY);
+        boolean isCheckedHumidity = getIntent().getExtras().getBoolean(HUMIDITY);
+        boolean isCheckedPressure = getIntent().getExtras().getBoolean(PRESSURE);
+        boolean isCheckedWindSpeed = getIntent().getExtras().getBoolean(WIND_SPEED);
+        TextView textViewCity = findViewById(R.id.city);
+        LinearLayout humidityLinear = findViewById(R.id.humidityLinear);
+        LinearLayout pressureLinear = findViewById(R.id.pressureLinear);
+        LinearLayout windSpeedLinear = findViewById(R.id.windSpeedLinear);
+        if (isCheckedHumidity) {
+            humidityLinear.setVisibility(View.VISIBLE);
+        } else {
+            humidityLinear.setVisibility(View.GONE);
+        }
+        if (isCheckedPressure) {
+            pressureLinear.setVisibility(View.VISIBLE);
+        } else {
+            pressureLinear.setVisibility(View.GONE);
+        }
+        if (isCheckedWindSpeed) {
+            windSpeedLinear.setVisibility(View.VISIBLE);
+        } else {
+            windSpeedLinear.setVisibility(View.GONE);
+        }
+        textViewCity.setText(city);
     }
 }
